7eb62fc4009854d27b64840b1df119e1688801e6
byronpople <byron.pople@gmail.com>
2020-02-11 14:46:07 +0000

    scaffolding pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_091526) do
+ActiveRecord::Schema.define(version: 2020_02_11_143147) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
