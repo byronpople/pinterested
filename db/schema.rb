d1398d7b99355e9608e86354cac18e446a4aafeb
byronpople <byron.pople@gmail.com>
2020-03-04 12:21:04 +0000

    db:migrate add_user_id_to_pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_143147) do
+ActiveRecord::Schema.define(version: 2020_03_04_120239) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,5 +31,4 @@ ActiveRecord::Schema.define(version: 2020_02_11_143147) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
