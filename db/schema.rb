80ab9ee45df83ee4e1a883f36814c3e29c2bcdfb
byronpople <byron.pople@gmail.com>
2020-03-05 14:54:56 +0000

    add user:id to pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_161820) do
+ActiveRecord::Schema.define(version: 2020_03_05_141037) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
