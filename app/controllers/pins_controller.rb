ef52c7168b32994f44280bc8364baeee5bb1edea
byronpople <byron.pople@gmail.com>
2020-04-20 13:51:00 +0100

    finished assign pins to user

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, expect: [:index, :show]
-  before_action :current_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /pins
   # GET /pins.json
   def index
@@ -43,7 +43,7 @@ class PinsController < ApplicationController
   def update
     respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.html { redirect_to @pin, notice: 'Pin was successfully updated' }
         format.json { render :show, status: :ok, location: @pin }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class PinsController < ApplicationController
   def destroy
     @pin.destroy
     respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
+      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed' }
       format.json { head :no_content }
     end
   end
@@ -71,5 +71,10 @@ class PinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: 'Not autherized to edit this pin' if @pin.nil?
     end
 end
